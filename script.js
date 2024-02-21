594a1d3b17c8e644125423222eb73920d038640a
AyushChaursiaya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-02-21 12:49:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function() {
-  // Function to write "DOM load success" to the body
-  function writeSuccessMessage() {
+ function writeSuccessMessage() {
     const successMessage = document.createElement("p");
     successMessage.textContent = "DOM load success";
     document.body.appendChild(successMessage);
   }
-	if (document.body.children.length === 0) {
-    // Write success message if body is empty
-    writeSuccessMessage();
-  } else {
-    // If body is not empty, clear it and then write success message
-    document.body.innerHTML = "";
-    writeSuccessMessage();
-  }
-});
+
+  // Call the function when DOM is loaded
+  writeSuccessMessage();
