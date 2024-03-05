7fa37ee9c799351bd7872c874c7b9ae7e3b830d9
Ayush Chaurasiya <147719677+AyushChaursiaya@users.noreply.github.com>
2024-03-05 22:39:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function() {
-	const body = document.querySelector("body");
-	body.innerText = "DOM load success";
-});
